90b9d2bfe3ae6f544d8378e96c10ec4419ca242b
Diego Armando Parada Castro <diegoarmandopc@ufps.edu.co>
2026-03-03 12:20:14 -0500

    Ajuste en el index.php para corregir el HTTP_USER_AGENT

--- index.php
+++ index.php
@@ -59,7 +59,8 @@ if ((in_array($_SERVER['REMOTE_ADDR'], array('::1', '127.0.0.1','10.0.2.2')) && 
 }
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
 
-$agentew = $_SERVER['HTTP_USER_AGENT'];
+//$agentew = $_SERVER['HTTP_USER_AGENT'];
+$agentew = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Desconocido';
 $findme   = '; wv';
 $pos = strpos($agentew, $findme);
 
